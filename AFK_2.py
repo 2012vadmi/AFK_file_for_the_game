86267da96d6c654b74ab7be6b2552ddc3027acd7
2012vadmi <miv873151@yandex.ru>
2025-03-09 14:28:56 +0300

    upgrade 0.0.1

--- AFK_2.py
+++ AFK_2.py
@@ -1,12 +1,14 @@
 import pyautogui
+import time as tm
 
-def AFK_mode_1():
+def AFK_mode():
     try:
         while True:
-            pyautogui.press('1')  # Нажимаем пробел  # Выводим сообщение в консоль
+            pyautogui.press(' ') # Нажимаем пробел  # Выводим сообщение в консоль
+            tm.sleep(60)
     except KeyboardInterrupt:
         print("Программа остановлена.")
 
 if __name__ == "__main__":
     print("Программа запущена. Нажмите Ctrl+C для выхода.")
-    AFK_mode_1()
+    AFK_mode()
